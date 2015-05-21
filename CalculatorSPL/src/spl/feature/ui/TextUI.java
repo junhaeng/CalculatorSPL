8a349ad9b8b5f9758c7b22fe7ce829fa65616a03
junhaeng <junhaeng777@naver.com>
2015-05-21 23:13:26 +0900

    Initail commit

---
+++ CalculatorSPL/src/spl/feature/ui/TextUI.java
@@ -0,0 +1,10 @@
+package spl.feature.ui;
+
+public class TextUI extends UI {
+
+    public static void main(String[] args) {
+        System.out.println("text ui version!!");
+        
+    }
+
+}
---
+++ CalculatorSPL/src/spl/feature/ui/UI.java
@@ -0,0 +1,10 @@
+package spl.feature.ui;
+
+public class UI {
+
+    public static void main(String[] args) {
+        // TODO Auto-generated method stub
+
+    }
+
+}
