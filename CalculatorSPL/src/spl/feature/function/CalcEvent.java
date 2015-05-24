9150ab17c4d992ed58b5b625ab85c0f301325e3d
junhaeng <junhaeng777@naver.com>
2015-05-24 12:02:24 +0900

    asdf

--- CalculatorSPL/src/spl/feature/function/CalcEvent.java
+++ CalculatorSPL/src/spl/feature/function/CalcEvent.java
@@ -1,12 +1,17 @@
 package spl.feature.function;
 
 public class CalcEvent {
-    private int eventType;
+    enum Type{
+        ZERO, ONE, TWO, THREE, FOUR, FIVE, SIX, SEVEN, EIGHT, NINE,
+        ADD, SUB, MUL, MOD, QUOT,
+        POW,LOG,INV,SQRT
+    }
+    private Type eventType;
     private String eventName;
-    public int getEventType() {
+    public Type getEventType() {
         return eventType;
     }
-    public void setEventType(int eventType) {
+    public void setEventType(Type eventType) {
         this.eventType = eventType;
     }
     public String getEventName() {
